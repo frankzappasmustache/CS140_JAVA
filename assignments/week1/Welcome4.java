662c4d0c03de5d62bfca3a9d5d75890eacd70d24
Dustin McClure <frankzappasmustache@frankzappaslip.karate>
2021-04-10 22:31:53 -0700

    pushing welcome files with header comment

---
+++ assignments/week1/Welcome4.java
@@ -0,0 +1,11 @@
+/*
+    Copyright 2021 <Dustin McClure>
+    Project Name: CS140_Java
+    Sub-project: Homework
+    File Name: Welcome4.java
+    Author: Dustin McClure
+    Lab: Chapter 1 examples
+    Modified Date: 04/10/2021
+    
+    Programs from lecture 1 being run for test purposes.
+*/
