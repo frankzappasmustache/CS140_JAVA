e68f5b2b82f6d1222e3863be74cc9116cfff901d
Dustin McClure <dustin.t.mcclure@gmail.com>
2021-04-11 21:46:30 -0700

    fixing folder typo

---
+++ assignments/week1/self_check/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+   public static void main(String[] args)
+   {
+      System.out.println("\"Several slashes are sometimes seen,\"");
+      System.out.println("said Sally, \"I've said so.\" See?");
+      System.out.println("\\ / \\\\ // \\\\\\ ///");
+   }
+}
