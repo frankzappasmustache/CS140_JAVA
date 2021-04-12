de5c64dc964f5d61c210f922db0fc9631ebb4e73
Dustin McClure <dustin.t.mcclure@gmail.com>
2021-04-11 21:27:21 -0700

    pushing finished Spikey.java and Egg.java

---
+++ assignments/week1/excercises/Egg.java
@@ -0,0 +1,26 @@
+/*
+    Copyright 2021 <Dustin McClure>
+    Project Name: CS140_Java
+    Sub-project: Chapter 1
+    File Name: Egg.java
+    Author: Dustin McClure
+    Lab: Excercise 9
+    Modified Date: 04/11/2021
+    
+    Implementation of excercise 9 which draws an egg
+*/
+
+public class Egg
+{
+   // main method begins execution
+   public static void main(String[] args)
+   {
+      System.out.println("  -------");
+      System.out.println(" /       \\");
+      System.out.println("/         \\");
+      System.out.println("-\"-'-\"-'-\"-");
+      System.out.println("\\         /");
+      System.out.println(" \\_______/");
+      // System.out.println("  -------");
+   } // end method main
+} // end class Egg
---
+++ assignments/week1/excercises/Spikey.java
@@ -0,0 +1,25 @@
+/*
+    Copyright 2021 <Dustin McClure>
+    Project Name: CS140_Java
+    Sub-project: Chapter 1
+    File Name: Spikey.java
+    Author: Dustin McClure
+    Lab: Excercise 2
+    Modified Date: 04/11/2021
+    
+    Implementation for excercise 2, which draws a spikey shape
+*/
+
+public class Spikey
+{
+   // main method begins execution
+   public static void main(String[] args)
+   {
+      System.out.println("  \\/");
+      System.out.println(" \\\\//");
+      System.out.println("\\\\\\///");
+      System.out.println("///\\\\\\");
+      System.out.println(" //\\\\");
+      System.out.println("  /\\");
+   } // end main method
+} // end class Spikey
