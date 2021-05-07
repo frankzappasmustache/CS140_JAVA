b4e9c22f60e60042d1e08d94ed440729c537a559
frankzappasmustache <dustin.t.mcclure@gmail.com>
2021-05-06 22:42:46 -0700

    pushing barebones cafewall and doodle java source files

---
+++ assignments/week5/lab_3/CafeWall.java
@@ -0,0 +1,27 @@
+/*
+    Copyright 2021 <Dustin McClure>
+    Project Name: CS140_Java
+    Sub-project: Lab 3
+    File Name: CafeWall.java
+    Author: Dustin McClure
+    Lab: Cafe Wall (Part B)
+    Modified Date: 05/06/2021
+*/
+import java.awt.*;
+
+public class CafeWall
+{
+    public static void main(String[] args) 
+    {
+        DrawingPanel panel = new DrawingPanel (650, 400);
+        panel.setBackground(Color.GRAY);
+    }
+
+    public static void drawLine(int x, int y, int pairs, int size) {
+
+    }
+
+    public static void drawGrid(int x, int y, int pairs, int size, int offset) {
+
+    }
+}
---
+++ assignments/week5/lab_3/DMDoodle.java
@@ -0,0 +1,19 @@
+/*
+    Copyright 2021 <Dustin McClure>
+    Project Name: CS140_Java
+    Sub-project: Lab 3
+    File Name: DMDoodle.java
+    Author: Dustin McClure
+    Lab: Doodle (Part A)
+    Modified Date: 05/06/2021
+*/
+import java.awt.*;
+
+public class DMDoodle
+{
+    public static void main(String[] args)
+    {
+        DrawingPanel panel = new DrawingPanel(500, 500);
+        
+    }
+}
