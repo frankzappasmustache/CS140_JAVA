1ca1e7c1c524dc2a3b8cd1613411a3cb21b00e40
frankzappasmustache <dustin.t.mcclure@gmail.com>
2021-05-07 22:16:57 -0700

    pushin cafewall and doodle with comments

--- assignments/week5/lab_3/CafeWall.java
+++ assignments/week5/lab_3/CafeWall.java
@@ -5,23 +5,66 @@
     File Name: CafeWall.java
     Author: Dustin McClure
     Lab: Cafe Wall (Part B)
-    Modified Date: 05/06/2021
+    Modified Date: 05/07/2021
+
+    A program that creates the cafe wall illusion using a method that draws row
+    pairs, a method that draws grids using series of row pairs, and the main method
+    which makes use of these two methods to *almost* reproduce the image from our lab.
+
+    I had a little trouble getting the spacing between rows and grids to show properly.
 */
 import java.awt.*;
 
+// Begin class CafeWall
 public class CafeWall
 {
-    public static void main(String[] args) 
+    // Method main begins execution
+    public static void main(String[] args)
     {
-        DrawingPanel dPanel = new DrawingPanel (650, 400);
-        panel.setBackground(Color.GRAY);
-    }
+        DrawingPanel dPanel = new DrawingPanel (650, 400); // create new drawing panel at 650x400
+        dPanel.setBackground(Color.GRAY); // set background color to gray
 
-    public static void drawLine(int x, int y, int pairs, int size) {
+        Graphics dPanelGraphics = dPanel.getGraphics(); // create new graphics object for drawing
 
-    }
+        makeRow(dPanelGraphics, 0, 0, 4, 20); // draw row 1
+        makeRow(dPanelGraphics, 50, 70, 5, 30); // draw row 2
+        makeGrid(dPanelGraphics, 10, 150, 4, 25, 0, 2); // draw bottom left row grid
+        makeGrid(dPanelGraphics, 250, 200, 3, 25, 10, 2); // draw bottom middle row grid
+        makeGrid(dPanelGraphics, 425, 180, 5, 20, 10, 2); // draw bottom right row grid
+        makeGrid(dPanelGraphics, 400, 20, 2, 35, 35, 2); // draw upper right row grid
 
-    public static void drawGrid(int x, int y, int pairs, int size, int offset) {
+    } // method main ends
+    
+    // Begin method makeRow
+    public static void makeRow(Graphics dPanelGraphics, int x, int y, int pairs, int size)
+    {
+        // for loop that draws 2 boxes and loops through to create as many pairs
+        // of boxes as necessary
+        for(int i = 0; i <= pairs; i++)
+        {
+            dPanelGraphics.setColor(new Color(0, 0, 0)); // set color for box 1 to black
+            dPanelGraphics.drawRect(x, y, size, size); // draw box 1 starting at x/y to size/size
+            dPanelGraphics.fillRect(x, y, size, size); // fill box 1 using identical coordinates and size
+            dPanelGraphics.setColor(new Color(0, 0, 255)); // change color to blue for x on black boxes
+            dPanelGraphics.drawLine(x, y, x + size, y + size); // draw line 1 of x in black box
+            x += size; // add size to x coord so that line 2 of the x starts at appopriate x coord
+            dPanelGraphics.drawLine(x, y + size, x + size, y); // draw line 2 of x in black box
+            dPanelGraphics.setColor(new Color(255, 255, 255)); // change color to white in preparation for white box
+            dPanelGraphics.drawRect(x, y, size, size); // draw box 2 starting at x/y to size/size
+            dPanelGraphics.fillRect(x, y, size, size); // fill box 2 using identical coordinates and size
+            x += size; // add size to x coord so the next pair in row start at the appropriate coordinates
+        } // end for loop
+    } // end method makeRow
 
-    }
-}
+    // Begin method makeGrid
+    public static void makeGrid(Graphics dPanelGraphics, int x, int y, int pairs, int size, int offset, int mortar)
+    {
+        // for loop that loops through while i is less than pairs * 2 (2 rows in each series)
+        for(int i = 0; i <= pairs * 2; i++)
+        {
+            // use makeRow method and account for offset/size of already existing rows
+            makeRow(dPanelGraphics, x + (i % 2 * offset), y + size * i, pairs, size);
+            y += 2; // account for mortar on next loop
+        } // end for loop
+    } // end method makeGrid
+} // end class CafeWall
--- assignments/week5/lab_3/DMDoodle.java
+++ assignments/week5/lab_3/DMDoodle.java
@@ -5,38 +5,47 @@
     File Name: DMDoodle.java
     Author: Dustin McClure
     Lab: Doodle (Part A)
-    Modified Date: 05/06/2021
+    Modified Date: 05/07/2021
+
+    A program that creates a little doodle with a few squares, an oval, 
+    and a triangle.
 */
 import java.awt.*;
 
+// Begin class DMDoodle
 public class DMDoodle
 {
+    // Method main begins execution
     public static void main(String[] args)
     {
-        int x[] = new int[3];
-        int y[] = new int[3];
+        int x[] = new int[3]; // declare an array of integers and initialize it
+        int y[] = new int[3]; // declare an array of integers and initialize it
 
-        x[0] = 100; y[0] = 100;
-        x[1] = 300; y[1] = 250;
-        x[2] = 350; y[2] = 350;
+        // set array indexes to appropriate x and y coordinates for triangle
+        x[0] = 100;
+        y[0] = 100;
+        x[1] = 300;
+        y[1] = 250;
+        x[2] = 350;
+        y[2] = 350;
 
-        DrawingPanel dPanel = new DrawingPanel(600, 600);
-        Graphics dPanelGraphics = dPanel.getGraphics();
+        DrawingPanel dPanel = new DrawingPanel(600, 600); // create new panel
+        Graphics dPanelGraphics = dPanel.getGraphics(); // create new graphics object
 
-        dPanel.setBackground(new Color(255,127,80));
-        dPanelGraphics.drawRect(0, 0, 300, 300);
-        dPanelGraphics.setColor(new Color(221,160,221));
-        dPanelGraphics.fillRect(0,0, 300, 300);
-        dPanelGraphics.setColor(new Color(218,165,32));
-        dPanelGraphics.fillRect(100, 100, 300, 300);
-        dPanelGraphics.drawRect(300, 300, 300, 300);
-        dPanelGraphics.setColor(new Color(102,205,170));
-        dPanelGraphics.fillRect(300, 300, 300, 300);
-        dPanelGraphics.drawOval(175, 175, 200, 200);
-        dPanelGraphics.setColor(new Color(72,209,204));
-        dPanelGraphics.fillOval(175, 175, 200, 200);
-        dPanelGraphics.drawPolygon(x, y, 3);
-        dPanelGraphics.setColor(new Color(255,99,71));
-        dPanelGraphics.fillPolygon(x, y, 3);
-    }
-}
+        dPanel.setBackground(new Color(255,127,80)); // set background color for panel
+        dPanelGraphics.drawRect(0, 0, 300, 300); // draw rectangle 1
+        dPanelGraphics.setColor(new Color(221,160,221)); // set bg color for rectangle 1
+        dPanelGraphics.fillRect(0,0, 300, 300); // fill rectangle 1
+        dPanelGraphics.setColor(new Color(218,165,32)); // set bg color for rect 2
+        dPanelGraphics.fillRect(100, 100, 300, 300); // fill rect 2 ( no drawing for this one :)
+        dPanelGraphics.drawRect(300, 300, 300, 300); // draw rect 3
+        dPanelGraphics.setColor(new Color(102,205,170)); // set bg color for rect 3
+        dPanelGraphics.fillRect(300, 300, 300, 300); // fill rect 3
+        dPanelGraphics.drawOval(175, 175, 200, 200); // draw oval
+        dPanelGraphics.setColor(new Color(72,209,204)); // set bg color for oval
+        dPanelGraphics.fillOval(175, 175, 200, 200); // fill oval
+        dPanelGraphics.drawPolygon(x, y, 3); // draw triangle using x and y coordinates
+        dPanelGraphics.setColor(new Color(255,99,71)); // set color for triangle
+        dPanelGraphics.fillPolygon(x, y, 3); // fill triangle
+    } // method main ends
+} // class DMDoodle ends
